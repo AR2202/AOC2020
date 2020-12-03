aee814e487cdaf7555aae97ae15ff3d71d9aa77b
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-12-03 11:50:10 +0000

    added day3 Part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Day3
 (
-solutionDay3a)
+solutionDay3a,
+solutionDay3b)
 where
 
 import Control.Lens
@@ -13,11 +14,15 @@ import Text.Read
 import Common
 import Data.List as L
 
+-------------------------------------------------------
 -- Types
 -------------------------------------------------------
 data Coord = Coord (Int,Int) deriving (Show, Read, Eq)
 type TreeMap = [String]
 
+-------------------------------
+-- Part 1
+-------------------------------
 
 multiplesof3 = map (3*) [0..]
 coordList ymax = map Coord $ zip multiplesof3 [0..ymax]
@@ -41,6 +46,36 @@ solutionDay3a = do
     let coord2check = coordList ymax
     let numtrees = howManyTrees repeatedPattern coord2check
     print numtrees
+
+---------------------------------------------------
+--Part 2
+---------------------------------------------------
+xs1 = [0..]
+xs2 = multiplesof3
+xs3 = map (5*) [0..]
+xs4 = map (7*) [0..]
+
+xslist = [xs1, xs2, xs3, xs4]
+coordlists = coordlist5 : map coordlist xslist
+
+coordlist  xs ymax = map Coord $ zip xs [0..ymax]
+
+coordlist5 ymax = map Coord $ zip [0..] $ map (2*)[0..ymax`div`2]
+
+-- | Solution to Day3 Part2
+solutionDay3b :: IO ()
+solutionDay3b = do
+    inputlines <- loadAndSplitLines "input3.txt"
+    let ymax = length inputlines -1
+    let repeatedPattern = map cycle inputlines
+    let coords = map ($ymax) coordlists
+    let numtrees = map (howManyTrees repeatedPattern) coords
+    let sol = product numtrees
+    print sol
+
+---------------------------------------------------
+--unrelated stuff
+--------------------------------------------------
 
 -- | Just a test, please ignore
 txt = "raindrops on roses and whiskers on kittens"
