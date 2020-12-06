746cd02bd74bce9b7573e96e46554f829eddab1d
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-12-06 11:44:49 +0000

    added Day6 Part 2

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,5 +1,6 @@
 module Day6 
-(solutionDay6a
+(solutionDay6a,
+solutionDay6b
 )
 where
 
@@ -7,8 +8,16 @@ import Common
 import Data.List
 import Control.Monad
 
-
+--------------------------
+--Part 1
+---------------------------
 
 solutionDay6a :: IO()
 solutionDay6a = splitOnBlankLine "input6.txt" >>= print . sum . fmap  (length . nub  . join . lines)
-    
+
+------------------------------
+--Part2
+------------------------------
+
+solutionDay6b :: IO()
+solutionDay6b = splitOnBlankLine "input6.txt" >>= print . sum . fmap  (length  . foldl1' intersect. lines)
