4d1499b1f18993f2a55452a9aae69b45001db6e4
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-12-06 21:11:42 +0000

    changed name of function

--- src/Day6.hs
+++ src/Day6.hs
@@ -14,11 +14,11 @@ import Control.Monad
 ---------------------------
 
 solutionDay6a :: IO()
-solutionDay6a = splitOn "\n\n" <$>readFile "input/input6.txt" >>= print . sum . fmap  (length . nub  . join . lines)
+solutionDay6a = splitOn "\n\n" <$> readFile "input/input6.txt" >>= print . sum . fmap  (length . nub  . join . lines)
 
 ------------------------------
 --Part2
 ------------------------------
 
 solutionDay6b :: IO ()
-solutionDay6b = splitOnBlankLine' "input6.txt" >>= print . sum . fmap  (length  . foldl1' intersect. lines)
+solutionDay6b = splitOnBlankLine "input6.txt" >>= print . sum . fmap  (length  . foldl1' intersect . lines)
