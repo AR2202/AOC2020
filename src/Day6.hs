ea81733596b2c175395e0166eb32362df59dc987
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-12-06 11:12:20 +0000

    added Day6 Part1

---
+++ src/Day6.hs
@@ -0,0 +1,14 @@
+module Day6 
+(solutionDay6a
+)
+where
+
+import Common
+import Data.List
+import Control.Monad
+
+
+
+solutionDay6a :: IO()
+solutionDay6a = splitOnBlankLine "input6.txt" >>= print . sum . fmap  (length . nub ) . fmap (join.lines)
+    
