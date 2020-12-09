6a1f2a31811de073743eb10cf3346584a2b99cf9
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-12-09 08:49:31 +0000

    added Day8 Part

--- src/Day8.hs
+++ src/Day8.hs
@@ -3,7 +3,9 @@
 module Day8 
 (
 example8,
-solutionDay8a)
+solutionDay8a,
+solutionDay8b,
+example8b)
 where
 
 import Common
@@ -23,9 +25,10 @@ data ProgramState = ProgramState {currIndex :: Ind,
                      currAcc :: Int,
                      previouslyRun :: [Ind]} deriving (Show, Read, Eq)
 
-runInstructions :: Program -> ProgramState -> Int
+runInstructions :: Program -> ProgramState -> Either Int Int
 runInstructions prog progstate 
-    |currIndex progstate `elem` previouslyRun progstate = currAcc progstate
+    |currIndex progstate `elem` previouslyRun progstate = Left $ currAcc progstate
+    |currIndex progstate >= length prog = Right $ currAcc progstate
     |currInstruction == NOP = runInstructions prog nextline
     |currInstruction == ACC = runInstructions prog accUpdated
     |currInstruction == JMP = runInstructions prog jumpline
@@ -79,3 +82,38 @@ example8 = applyProgram "example8.txt"
 solutionDay8a :: IO ()
 solutionDay8a = applyProgram "input8.txt"
 
+---------------------------------------
+--Part 2
+-------------------------------------
+
+changeInstruction :: Instruction -> Instruction
+changeInstruction JMP = NOP
+changeInstruction NOP = JMP
+changeInstruction   x = x
+
+changeInstructionLine :: InstructionLine -> InstructionLine
+changeInstructionLine (x,y) = (changeInstruction x, y)
+
+changeNthelem list n = (take n list) ++ changed:(drop (n+1) list)
+    where changed = changeInstructionLine $list !! n
+
+allPrograms prog = [changeNthelem prog n | n<-[0..length prog -1], fst(prog !! n) == NOP || fst(prog !! n) == JMP]
+
+findTerminating proglist = case (runInstructions (head proglist) initialState) of 
+    (Right x) -> x
+    (Left x) -> findTerminating (tail proglist) 
+
+applyModifiedProgram :: String -> IO ()
+applyModifiedProgram input = do
+    lines <-loadAndSplitLines input
+    let prog = toInstructionLines lines
+    let allProg = allPrograms prog
+    let accumulator = findTerminating allProg
+    
+    print accumulator
+
+example8b :: IO ()
+example8b = applyModifiedProgram "example8.txt"
+
+solutionDay8b :: IO ()
+solutionDay8b = applyModifiedProgram "input8.txt"
