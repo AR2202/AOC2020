3bb354ed3f0f287de4ff44cb1fc4c12e9c8051b5
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-12-21 12:02:01 +0000

    added Day 20 Part 1

---
+++ src/Day19.hs
@@ -0,0 +1,150 @@
+module Day19
+(testRule1,
+testRule2,
+testRule3,
+testRule4,
+testRule5,
+testRule6,
+example19a
+
+)
+where
+import Common
+import Data.List as L
+import Data.IntMap.Strict as IM
+import Data.List.Split(splitOn)
+import Text.Parsec (ParseError,parse,eof,try)
+import Text.Parsec.String (Parser)
+import Text.Parsec.Char
+import Data.Char
+import Text.Parsec.Combinator (many1, choice, chainl1)
+import Control.Applicative ((<|>), many)
+import Control.Monad (void)
+import Data.Either.Unwrap(fromRight)
+import Data.Maybe(fromJust)
+
+
+----------------------------------------
+--Types
+----------------------------------------
+
+        
+data Rule = Num Int
+          | AndThen Rule Rule
+          | OrElse Rule Rule
+          | Str String
+                  deriving (Eq,Show)
+
+------------------
+--Parser
+------------------
+
+term :: Parser Rule -> Parser Rule
+term rule = numE <|> strE 
+
+whitespace :: Parser ()
+whitespace = void $ many $ oneOf " \n\t\""
+
+
+parseWithEof :: Parser a -> String -> Either ParseError a
+parseWithEof p = parse (p <* eof) ""
+
+parseWithWhitespace :: Parser a -> String -> Either ParseError a
+parseWithWhitespace p = parseWithEof wrapper
+  where
+    wrapper = do
+        whitespace
+        p
+
+lexeme :: Parser a -> Parser a
+lexeme p = do
+           x <- p
+           whitespace
+           return x
+
+numE :: Parser Rule
+numE = do
+    n <- lexeme $ many1 digit
+    return $ Num $ read n
+
+strE :: Parser Rule
+strE = lexeme $ do
+    fc <- firstChar
+    rest <- many nonFirstChar
+    return $ Str (fc:rest)
+  where
+    firstChar = satisfy (\a -> isLetter a || a == '_')
+    nonFirstChar = satisfy (\a -> isDigit a || isLetter a || a == '_')
+
+
+
+parseRule :: Parser Rule
+parseRule = chainl1 term8 (try orelse <|> try andthen)
+  where
+    orelse = do
+      
+        void $ lexeme $ char '|'
+       
+        return OrElse
+    andthen = do
+        void $ many $ oneOf " \n\t"
+        return AndThen
+    term8 = term parseRule
+
+parseRule' :: Parser Rule
+parseRule' = do
+   
+    e <- term7
+   
+    maybeAddSuffix e
+  where
+   
+    addSuffix e0 = do
+        
+        e1 <- term7
+        maybeAddSuffix  (AndThen e0 e1)
+    orelse e0 = do
+        void $ lexeme $ char '|'
+        e1 <- term7
+        e2 <- maybeAddSuffix e1
+        maybeAddSuffix (OrElse e0 e2)
+    
+    maybeAddSuffix' e = orelse e <|> addSuffix e <|> return e
+    maybeAddSuffix e = addSuffix e <|> orelse e <|> return e
+    
+
+term7 :: Parser Rule
+term7 = term parseRule'
+
+testRule1 = parseWithWhitespace parseRule' "\"a\""
+testRule2 = parseWithWhitespace parseRule' "1 2 "
+testRule3 = parseWithWhitespace parseRule' "1 3 | 3 1"
+testRule4 = parseWithWhitespace parseRule' "1  | 3 "
+testRule5 = parseWithWhitespace parseRule' "1  | 3 1"
+testRule6 = parseWithWhitespace parseRule' "1 3 | 1"
+
+parseSnd :: (a,String) -> (a,  Rule)
+parseSnd tuple = (fst tuple,fromRight $parseWithWhitespace parseRule' $snd tuple)
+
+toTupleFirstToInt :: [String] -> (Int,String)
+toTupleFirstToInt list = (read $ head list, last list)
+
+lookupRule :: IntMap Rule -> Int -> Rule
+lookupRule intmap key = eval $fromJust $ IM.lookup key intmap
+    where eval  (Num i) = lookupRule intmap i
+          eval  (OrElse x y) = OrElse (eval x) (eval y)
+          eval  (AndThen x y) = AndThen (eval x) (eval y)
+          eval  (Str s) = Str s
+
+
+example19a :: IO()
+example19a = do
+    input <- splitOnBlankLine "example19.txt"
+    let ruleinput = L.map  (splitOn ":" ) $ lines $ head input
+    let rules = L.map toTupleFirstToInt ruleinput
+    let parsed = L.map parseSnd rules
+    let messages = lines $ last input
+    let rulemap = L.foldl' (flip (uncurry IM.insert)) empty parsed
+    let evaluatedRules = lookupRule rulemap 0
+    print evaluatedRules
+    print messages
---
+++ src/Day20.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-#LANGUAGE BangPatterns#-}
+
+module Day20
+(example20,
+solutionDay20a
+
+)
+where
+
+import Common
+import qualified Data.Set as S
+import Data.Maybe(fromJust)
+import Control.Lens
+import Data.List (nub,(\\))
+import Control.Lens.Regex.Text
+import Text.RawString.QQ
+import qualified Data.Text as T
+
+example20 :: IO ()
+example20 = part1 "example20.txt"
+
+solutionDay20a :: IO ()
+solutionDay20a = part1 "input20.txt"
+
+part1 :: String -> IO ()
+part1 filename = do
+    tiles <-splitOnBlankLine filename
+    let borders = map makeBorder tiles
+    let tileids = map makeTileId tiles
+    let tilelist = zip tileids borders
+    let corners = map fst $ filter (sndIsCornerTile borders) tilelist
+    let result = product corners
+    print result
+    
+matchDigits :: String -> Maybe T.Text
+matchDigits txt = T.pack txt ^? [regex|(\d+)|] . match
+
+readToInt :: T.Text -> Int
+readToInt txt = read $ T.unpack txt
+
+makeBorder line = [top,bottom,right,left]
+    where top = head $ drop 1 $ lines line
+          bottom = last $ lines line
+          right = map last $ drop 1 $lines line
+          left = map head $ drop 1$lines line
+
+makeTileId line = readToInt $ fromJust $ matchDigits $ head $ lines line
+
+isCornerTile :: [[String]] -> [String] -> Bool
+isCornerTile tilelist tile = (length . filter notMatching) tile == 2
+    where notMatching x = x `notElem` tileset && reverse x `notElem` tileset
+          tileset = nub $ concat $ tilelist \\ [tile]
+
+sndIsCornerTile tilelist tile = isCornerTile tilelist $ snd tile
