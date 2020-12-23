97ff0b3f3b681f18ea107a7c2b23c7435cab963b
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-12-23 08:41:26 +0000

    changed Part2 Day 22)

--- src/Day22.hs
+++ src/Day22.hs
@@ -37,7 +37,7 @@ recursiveCombat :: [Int]->[Int]-> [[Int]] -> [[Int]]-> Int
 recursiveCombat [] list _ _ = sum $ zipWith (*) [1..] $ reverse list
 recursiveCombat list [] _ _ = sum $ zipWith (*) [1..] $ reverse list
 recursiveCombat p1 p2 prev1 prev2
-    |p1 `elem` prev1 && p2 `elem` prev2 = sum $ zipWith (*) [1..] $ reverse p1
+    |(p1,p2) `elem` (zip prev1  prev2) = sum $ zipWith (*) [1..] $ reverse p1
     |head p1 < length p1 && head p2 < length p2 = if subgame sub1 sub2 [] [] then recursiveCombat (tail p1 ++ [head p1, head p2]) (tail p2) (p1:prev1) (p2:prev2) else recursiveCombat (tail p1) (tail p2 ++ [head p2, head p1])(p1:prev1)(p2:prev2)
     
     |head p1 > head p2 = recursiveCombat (tail p1 ++ [head p1, head p2]) (tail p2) (p1:prev1)(p2:prev2)
@@ -49,7 +49,7 @@ recursiveCombat p1 p2 prev1 prev2
 subgame [] list _ _ = False
 subgame list [] _ _ = True
 subgame p3 p4 prev1 prev2
-    |p3 `elem` prev1 && p4 `elem` prev2 = True
+    |(p3,p4) `elem` (zip prev1 prev2) = True
     |head p3 > head p4 = subgame (tail p3 ++ [head p3, head p4]) (tail p4) (p3:prev1) (p4:prev2)
     |otherwise         = subgame (tail p3) (tail p4 ++ [head p4, head p3]) (p3:prev1) (p4:prev2)
 
