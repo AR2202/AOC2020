db678d2a3408e76e3d656249eb1747cfaca374fe
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-12-23 08:19:35 +0000

    changed Part2 Day22

--- src/Day22.hs
+++ src/Day22.hs
@@ -33,23 +33,25 @@ cardGame p1 p2
 --Part2
 ------------
 
-recursiveCombat :: [Int]->[Int]-> [[Int]] -> Int
-recursiveCombat [] list _ = sum $ zipWith (*) [1..] $ reverse list
-recursiveCombat list [] _ = sum $ zipWith (*) [1..] $ reverse list
-recursiveCombat p1 p2 prev
-    |p1++p2 `elem` prev = sum $ zipWith (*) [1..] $ reverse p1
-    |head p1 < length p1 && head p2 < length p2 = if subgame (take (head p1) (tail p1)) (take (head p2)(tail p2)) [] then recursiveCombat (tail p1 ++ [head p1, head p2]) (tail p2) ((p1++p2):prev) else recursiveCombat (tail p1) (tail p2 ++ [head p2, head p1])((p1++p2):prev)
-    |head p1 > head p2 = recursiveCombat (tail p1 ++ [head p1, head p2]) (tail p2) ((p1++p2):prev)
-    |otherwise         = recursiveCombat (tail p1) (tail p2 ++ [head p2, head p1]) ((p1++p2):prev)
-
+recursiveCombat :: [Int]->[Int]-> [[Int]] -> [[Int]]-> Int
+recursiveCombat [] list _ _ = sum $ zipWith (*) [1..] $ reverse list
+recursiveCombat list [] _ _ = sum $ zipWith (*) [1..] $ reverse list
+recursiveCombat p1 p2 prev1 prev2
+    |p1 `elem` prev1 && p2 `elem` prev2 = sum $ zipWith (*) [1..] $ reverse p1
+    |head p1 < length p1 && head p2 < length p2 = if subgame sub1 sub2 [] [] then recursiveCombat (tail p1 ++ [head p1, head p2]) (tail p2) (p1:prev1) (p2:prev2) else recursiveCombat (tail p1) (tail p2 ++ [head p2, head p1])(p1:prev1)(p2:prev2)
+    
+    |head p1 > head p2 = recursiveCombat (tail p1 ++ [head p1, head p2]) (tail p2) (p1:prev1)(p2:prev2)
+    |otherwise         = recursiveCombat (tail p1) (tail p2 ++ [head p2, head p1]) (p1:prev1) (p2:prev2)
+        where sub1 = take (head p1) (tail p1)
+              sub2 = take (head p2)(tail p2)
             
             
-subgame [] list _ = False
-subgame list [] _ = True
-subgame p3 p4 prev'
-    |p3++p4 `elem` prev' = True
-    |head p3 > head p4 = subgame (tail p3 ++ [head p3, head p4]) (tail p4) ((p3++p4):prev')
-    |otherwise         = subgame (tail p3) (tail p4 ++ [head p4, head p3]) ((p3++p4):prev')
+subgame [] list _ _ = False
+subgame list [] _ _ = True
+subgame p3 p4 prev1 prev2
+    |p3 `elem` prev1 && p4 `elem` prev2 = True
+    |head p3 > head p4 = subgame (tail p3 ++ [head p3, head p4]) (tail p4) (p3:prev1) (p4:prev2)
+    |otherwise         = subgame (tail p3) (tail p4 ++ [head p4, head p3]) (p3:prev1) (p4:prev2)
 
 example22b :: IO ()
 example22b = part2 "example22.txt"
@@ -62,6 +64,6 @@ part2 filename = do
     players <- splitOnBlankLine filename
     let player1Deck = map read $ drop 1 $ lines $ head players
     let player2Deck = map read $ drop 1 $ lines $ last players
-    let winningScore = recursiveCombat player1Deck player2Deck []
+    let winningScore = recursiveCombat player1Deck player2Deck [][]
     print winningScore
     
